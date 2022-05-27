856e7c91b54251598dc260cf15636b09a20a81e2
Bakugan1024 <85323092+Bakugan1024@users.noreply.github.com>
2022-05-27 19:51:38 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-objectDetector= "";
+objectDetector = "";
 
 img = "";
-objects = [];
 status = "";
+objects = [];
 
-function preload(){
+function preload() {
   img = loadImage('dog_cat.jpg');
 }
 
@@ -30,19 +30,19 @@ function gotResult(error, results) {
   objects = results;
 }
 
-function draw()
-{
-    image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
+function draw() {
+  image(img, 0, 0, 640, 420);
 
+  if (status != "") {
+    for (i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML = "Status:Object Detected";
 
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
+      fill("#FF0000");
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + "" + "%", objects[i].x + 15, objects[i].y + 15);
+      noFill();
+      stroke("#FF0000");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
 }
